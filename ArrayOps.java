d42dbcc07a5f49129a268053ba0ed0a255079d28
Amit.manoshevitz <Amit.manoshevitz@post.runi.ac.il>
2024-01-15 18:51:49 +0200

    first

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,11 +1,27 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
+   
+}
     
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+    public static int findMissingInt (int[] array) {
+        int len = array.length;
+        int i = 0;
+        int j = 0;
+
+        while (i<len) {
+            while (j<len) {
+               if(array[j] == i){
+                j++;
+                i++;
+               }
+                else if(array[j] != i){
+                    j++;
+                }
+            }
+           
+            i++;
+        }
+        return i;
     }
 
     public static int secondMaxValue(int [] array) {
